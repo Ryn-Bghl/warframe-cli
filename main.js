82b024c986f884630e76ff295e3dde642300b1ed
_Duck-Noris_ <rayan.boughalia@gmail.com>
2025-10-07 13:05:06 +0200

    added title modules

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
 import { autoCorrect } from "./autoCorrect.js";
 import { formatResults } from "./formatCode.js";
 import { setOrder, getItemId, getJWT, getItems } from "./api.js";
+import { setTitle } from "./title.js";
 
 async function main() {
+  setTitle();
   const JWT = await getJWT();
 
   // Utilisation avec une liste statique
   const items = await getItems();
 
-  const input = "nekros prime blueprint";
+  const input = "nkr pr st";
   const results = autoCorrect(input, items, 5);
   console.log("Résultats formatés:\n", formatResults(results));
   console.log(
+    // TOFIX: NOT PRACTICAL
     await setOrder(JWT, {
       type: "sell",
       itemId: await getItemId("nekros_prime_set"),
       quantity: 1,
       platinum: 50,
-      visible: true,
+      visible: false,
     })
   );
 }
